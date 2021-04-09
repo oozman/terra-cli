7af9aeec69e78d7dfc007f80f507c212dc97e1be
Omar <oomarusman@gmail.com>
2021-04-09 18:58:25 +0800

    Add `get-anchor-apy` command.

--- index.js
+++ index.js
@@ -18,4 +18,15 @@ program
         console.log(stringify(await Terra.wallet(address, options)));
     });
 
+program
+    .command("get-anchor-apy")
+    .description("Get current Anchor APY.")
+    .option("-l, --lcd-url <value>", "Set LCD URL.", "https://tequila-lcd.terra.dev")
+    .option("-c, --chain-id <value>", "Set Chain ID.", "tequila-0004")
+    .option("-a, --address-provider-id <value>", "Set Address Provider ID.", "tequila0004")
+    .option("-d, --denom <value>", "Set Market Denomination. [uusd, ukrw]", "uusd")
+    .action(async (options) => {
+        console.log(stringify(await Terra.getAnchorAPY(options)));
+    });
+
 program.parse(process.argv);
